1a42fa7ee58aa3f5e692145295887452b3284d21
MINTYoo <107981047+MINTYoo@users.noreply.github.com>
2024-01-09 20:13:18 -0800

    Update client.py

--- client.py
+++ client.py
@@ -25,10 +25,9 @@ def parse_message(message):
 
 
 def checkClientMac(macAdd):
-    if MAC == macAdd:
-        return True
-    else:
-        return False
+    return macAdd== MAC
+      
+
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSocket.settimeout(8)  # Set a timeout value in seconds
@@ -46,8 +45,8 @@ def checkTimeStamp(time):
         return True
     else:
         return False
-    
-    
+
+
 def handleDiscover(message):
     response = message[0][:-1]
     clientmac = message[1]
@@ -59,7 +58,7 @@ def handleDiscover(message):
                 request_message = f"REQUEST {clientmac} {clientIP} {clientTimeStamp}"
                 clientSocket.sendto(request_message.encode(), ("127.0.0.1", 9000))
                 ACK_response, _ = clientSocket.recvfrom(4096)
-                print(f"Received: {ACK_response.decode()}")
+                print(f"Received request: {ACK_response.decode()}")
         else:
             print("Timestamp has expired. Exiting.")
             quit()
@@ -69,44 +68,55 @@ def handleDiscover(message):
             quit()
         else:
             #ACK_response, _ = clientSocket.recvfrom(4096) // possible error
-            print(f"Received: {message}")
+            print("Received:", ' '.join(message))
+    elif(response == "DECLINE"):
+        print("system resources are exhausted. Please try later.")
+        quit()
 try:
     # Sending DISCOVER message
     discover_message = f"DISCOVER {MAC} "
     print(f"Sending: {discover_message}")
     clientSocket.sendto(discover_message.encode(), ("127.0.0.1", 9000))
-    
+
     # Print received response
     response, _ = clientSocket.recvfrom(4096)
-    print(f"Received Response: {response.decode()}")
+    print(f"Received : {response.decode()}")
     parsed_message = parse_message(response)
     handleDiscover(parsed_message)
-     #print(f"Received: {response.decode()}")
     while True:
         print("Choose an option:")
         print("1. Renew")
         print("2. Release")
-        print("3. List")
-        print("4. Quit")
+        print("3. Quit")
 
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            renew_message = f"RENEW {MAC} {datetime.now().isoformat()}"
+            Time = f"{parsed_message[3]} {parsed_message[4]}"
+            renew_message = f"RENEW {MAC} {parsed_message[2]} {Time}"
             clientSocket.sendto(renew_message.encode(), ("127.0.0.1", 9000))
             renew_response, _ = clientSocket.recvfrom(4096)
+            if(renew_response.decode() == "DISCOVER"):
+                #send message
+                discover_message = f"DISCOVER {MAC} "
+                print(f"Sending: {discover_message}")
+                clientSocket.sendto(discover_message.encode(), ("127.0.0.1", 9000))
+
+                # Print received response
+                response, _ = clientSocket.recvfrom(4096)
+                print(f"Received Response: {response.decode()}")
+                parsed_message = parse_message(response)
+                handleDiscover(parsed_message)
+            elif(renew_response.decode() == "OFFER"):
+                handleDiscover(parsed_message)
             print(f"Received: {renew_response.decode()}")
         elif choice == "2":
-            release_message = f"RELEASE {MAC} {datetime.now().isoformat()}"
+            Time = f"{parsed_message[3]} {parsed_message[4]}"
+            release_message = f"RELEASE {MAC} {parsed_message[2]} {Time}"
             clientSocket.sendto(release_message.encode(), ("127.0.0.1", 9000))
             release_response, _ = clientSocket.recvfrom(4096)
             print(f"Received: {release_response.decode()}")
         elif choice == "3":
-            List = f"LIST"
-            clientSocket.sendto(List.encode(), ("127.0.0.1", 9000))
-            list_response, _ = clientSocket.recvfrom(4096)
-            print(f"list_response: {list_response.decode()}")
-        elif choice == "4":
             print("Quitting the client program.")
             break
         else:
